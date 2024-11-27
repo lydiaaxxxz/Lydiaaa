566eba9c963b93977f14a44b8a662c9d84b2ee10
lydiaaxxxz <celynnxx0@gmail.com>
2024-11-27 16:09:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "lydiacantik" ]; then
+  if [ "$USER_TOKEN" = "lydiaa" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Lydia${NC}"
